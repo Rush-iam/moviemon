f797cc4417a05bf51a43bab49c6655257608e054
Rush-iam <rushiam@gmail.com>
2022-05-22 19:15:14 +0300

    - WorldMap view - done!

--- moviemon/views.py
+++ moviemon/views.py
@@ -13,34 +13,62 @@ class MapState(Enum):
     BALL = 2
 
 
+class BattleState(Enum):
+    NONE = 0
+    MISSED = 1
+
+
 class GameDummy:
     class Player:
         pos_x = 3
         pos_y = 5
         movieballs = 2
+        caught = set()
+        strength = 3.2
+
+        def calc_win_rate(self, movimon_rating):
+            return 33
 
     player = Player()
     map_size_x = 10
     map_size_y = 10
     moviemons = {
-        123: 'Godzilla'
+        123: {
+            'title': 'Godzilla',
+            'rating': 3.3,
+            'poster': 'posters/godzilla.jpg'
+        }
     }
 
     map_state: MapState = MapState.NONE
     map_state_moviemon = 123
 
+    battle_state: BattleState = BattleState.NONE
+
     def __init__(self):
         random.seed()
+
+    def get_movie(self, movie_id: int):
+        return self.moviemons[movie_id]
+
+    def try_to_catch(self, movie_id: int):
+        if random.randrange(4) == 1:  # DO CALC BY SUBJECT
+            self.player.caught.add(movie_id)
+        else:
+            self.battle_state = BattleState.MISSED
 
     def load_default_settings(self):
         self.map_state = MapState.NONE
 
     def set_random_map_state(self):
-        self.map_state = random.choice((MapState.MON, MapState.BALL))
-        if self.map_state == MapState.MON:
-            self.map_state_moviemon = random.choice(tuple(self.moviemons.keys()))
+        self.map_state = random.choice((MapState.NONE, MapState.MON, MapState.BALL))
+        not_caught = [m for m in self.moviemons if m not in self.player.caught]
+        if self.map_state == MapState.MON and not_caught:
+            self.map_state_moviemon = random.choice(not_caught)
         elif self.map_state == MapState.BALL:
             self.player.movieballs += 1
+        else:
+            self.map_state = MapState.NONE
 
     def move_up(self):
         if self.player.pos_y != 0:
@@ -80,7 +108,6 @@ def title(request: HttpRequest) -> HttpResponse:
         'title': 'Title Screen',
         'buttons_active': {'A', 'B'},
     }
-
     return render(request, 'moviemon/title.html', context)
 
 
@@ -98,6 +125,8 @@ def worldmap(request: HttpRequest) -> HttpResponse:
             case 'A':
                 if game.map_state == MapState.MON:
                     moviemon_id = game.map_state_moviemon
+                    game.map_state = MapState.NONE
+                    game.battle_state = BattleState.NONE
                     return redirect(f'battle/{moviemon_id}')
             case 'Select':
                 return redirect('moviedex')
@@ -135,3 +164,41 @@ def worldmap(request: HttpRequest) -> HttpResponse:
         context['buttons_active'].remove('A')
 
     return render(request, 'moviemon/worldmap.html', context)
+
+
+def battle(request: HttpRequest, moviemon_id: int) -> HttpResponse:
+    if request.method == 'POST':
+        match pressed_button(request.POST):
+            case 'A':
+                if game.player.movieballs > 0:
+                    game.player.movieballs -= 1
+                    game.try_to_catch(moviemon_id)
+            case 'B':
+                return redirect('worldmap')
+        return redirect(request.resolver_match.view_name, moviemon_id)
+
+    movie = game.get_movie(moviemon_id)
+
+    context = {
+        'title': 'Battle!',
+        'buttons_active': {'A', 'B'},
+        'movieballs': game.player.movieballs,
+        'moviemon_strength': movie['rating'],
+        'moviemon_img': movie['poster'],
+        'player_strength': game.player.strength,
+        'win_rate': game.player.calc_win_rate(movie['rating'])
+    }
+
+    if moviemon_id in game.player.caught:
+        context['buttons_active'].remove('A')
+        context['message'] = 'You caught it!'
+    else:
+        if game.player.movieballs == 0:
+            context['buttons_active'].remove('A')
+            context['message'] = 'No movieballs left :-('
+        else:
+            context['message'] = '<b>A</b> - Launch movieball!'
+        if game.battle_state == BattleState.MISSED:
+            context['message'] += '<br>You missed!'
+
+    return render(request, 'moviemon/battle.html', context)
